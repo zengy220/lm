a64f22605497b537c5c3ef060f94aae49425fb7d
zengy <1635942631@qq.com>
2018-12-21 16:28:02 +0800

    【

--- Application/Web/Controller/ApiController.class.php
+++ Application/Web/Controller/ApiController.class.php
@@ -28,7 +28,7 @@ class ApiController extends CommonController
 		// $news=M("content_content")->where($map)->order('istop desc,create_time desc,update_time desc')->limit($p->limit[0],$p->limit[1])->select();
 		// 默认是第一个
 		$page = empty(I('page'))?1:I('page');
-		$each =2;
+		$each =4;
 		$news=M("content_content")->where($map)->order('istop desc,create_time desc,id desc,update_time desc')->limit($each*($page-1),$each)->select();
 
 
@@ -57,7 +57,7 @@ class ApiController extends CommonController
         ),JSON_UNESCAPED_UNICODE);
         
         //转换成字符串JSON
-        print($json);exit;
+        // print($json);exit;
 
 
 		// var_dump($news);exit;
