9888836e098d05f01ba50df9fea28db2b05aad0b
zengy <1635942631@qq.com>
2018-12-25 17:23:25 +0800

    6

--- Application/Web/Controller/ApiController.class.php
+++ Application/Web/Controller/ApiController.class.php
@@ -22,17 +22,16 @@ class ApiController extends CommonController
 	//带分页内容获取控制器
     public function page_content()
     {
-    	session_start();
-		// 是否存在请求源
-		if(isset($_SERVER["HTTP_ORIGIN"])) {
-		    header('Access-Control-Allow-Origin:'.$_SERVER["HTTP_ORIGIN"]);  
-		}
-		header('Access-Control-Allow-Origin:null');  
-		header('Access-Control-Allow-Methods:OPTIONS, GET, POST');
+    	header('Access-Control-Allow-Methods:OPTIONS, GET, POST');
 		header('Access-Control-Allow-Headers:x-requested-with');
-		header('Access-Control-Max-Age:86400');
-
+		header('Access-Control-Max-Age:86400');  
+		header('Access-Control-Allow-Origin:'.$_SERVER['HTTP_ORIGIN']);
 		header('Access-Control-Allow-Credentials:true');
+		header('Access-Control-Allow-Methods:GET, POST, PUT, DELETE, OPTIONS');
+		header('Access-Control-Allow-Headers:x-requested-with,content-type');
+		header('Access-Control-Allow-Headers:Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With');
+
+
 
 
 
